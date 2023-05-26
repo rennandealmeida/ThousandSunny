7faac3fbb2f6ec10c6305c926ba8996298d72fc7
rennandealmeida <dealmeida.rennan@gmail.com>
2023-05-26 17:32:22 -0300

    Adjusting files

---
+++ api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+
+app = Flask("ThousandSunny",static_folder="./images")
+
+@app.route("/", methods=["GET"])
+def Home():
+    return render_template('thousandsunny.html')
+
+@app.route("/luffy")
+def FraseLuffy():
+    return render_template('luffy.html')
+
+@app.route("/zoro", methods=["GET"])
+def FraseZoro():
+    return render_template('zoro.html')
+
+@app.route("/sanji", methods=["GET"])
+def FraseSanji():
+    return render_template('sanji.html')
+
+@app.route("/usopp", methods=["GET"])
+def FraseUsopp():
+    return render_template('usopp.html')
+
+@app.route("/nami", methods=["GET"])
+def FraseNami():
+    return render_template('nami.html')
+
+app.run(debug=True, host='0.0.0.0')
